804f681513ffbcf4bd2be0e0d0418b752340e146
FlipOwz <felipegalvaosp@gmail.com>
2024-09-09 16:27:15 -0300

    aba Serviços adicionada

---
+++ scripts/animations.js
@@ -0,0 +1,16 @@
+// Função para detectar quando os elementos entram na viewport
+function handleScroll() {
+    const elements = document.querySelectorAll('.fade-in');
+    
+    elements.forEach(element => {
+      const rect = element.getBoundingClientRect();
+      
+      if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+        element.classList.add('show');
+      }
+    });
+  }
+  
+  // Adiciona o evento de scroll
+  window.addEventListener('scroll', handleScroll);
+  
